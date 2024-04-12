dd3abaa773db7aa2cdad3c6f086a359bf51d12f9
Eric <166804530+ea33064@users.noreply.github.com>
2024-04-12 10:58:57 +0000

    after template sync

--- project/app.js
+++ project/app.js
@@ -6,6 +6,9 @@ const app = express();
 // Define port and use 3000 as default
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+app.use(express.static("src/views"));
+
 app.get("/", (req, res) => {
   res.send("Hello Friends!");
 });
